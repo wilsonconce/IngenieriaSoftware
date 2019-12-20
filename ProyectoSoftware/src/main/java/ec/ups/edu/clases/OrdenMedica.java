2e6ba418a1d40d26efc35407ae7219110af4cecd
wilsonconce <49213252+wilsonconce@users.noreply.github.com>
2019-12-20 11:42:42 -0500

    agregar modelos
    new calsss

---
+++ ProyectoSoftware/src/main/java/ec/ups/edu/clases/OrdenMedica.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.ups.edu.clases;
+
+/**
+ *
+ * @author wilso
+ */
+public class OrdenMedica {
+    
+    private int idOrdenMedica;
+    private String tipo;
+}
