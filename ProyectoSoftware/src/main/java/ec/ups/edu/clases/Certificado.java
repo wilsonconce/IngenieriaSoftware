27c75c9a3810665ca6a9b3723b2c2cc80741849a
wilsonconce <49213252+wilsonconce@users.noreply.github.com>
2019-12-20 11:33:25 -0500

    Create Certificado.java
    nueva clses

---
+++ ProyectoSoftware/src/main/java/ec/ups/edu/clases/Certificado.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.ups.edu.clases;
+
+import java.util.Date;
+
+/**
+ *
+ * @author wilso
+ */
+public class Certificado {
+    
+    private int idCertificado;
+    private Date fechaEmision;
+    private String descripcion;
+}
