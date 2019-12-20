f494849664109d6d3f07c38bb21f94b9d095123a
wilsonconce <49213252+wilsonconce@users.noreply.github.com>
2019-12-20 11:04:39 -0500

    create clases
    nuevas clases

---
+++ ProyectoSoftware/src/main/java/ec/ups/edu/clases/Medicamento.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.ups.edu.clases;
+
+/**
+ *
+ * @author wilso
+ */
+public class Medicamento {
+
+    private int idMedicamento;
+    private String nombre;
+    private String dosis;
+}
