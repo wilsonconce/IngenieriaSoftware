7d07a109dde2a961efa4b0088e8579948d30fdce
DavidEF1996 <59062590+DavidEF1996@users.noreply.github.com>
2020-01-03 11:30:34 -0500

    controlador medicamento maven

--- ProyectoSoftware/src/main/java/ec/ups/edu/clases/Medicamento.java
+++ ProyectoSoftware/src/main/java/ec/ups/edu/clases/Medicamento.java
@@ -1,41 +1,69 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ec.ups.edu.clases;
 
-/**
- *
- * @author wilso
- */
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="medicamento")
 public class Medicamento {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private String idMedicamento;
+	@Column
+	private String nombre;
+	@Column
+	private String dosis;
 
-    private String idMedicamento;
-    private String nombre;
-    private String dosis;
 
-    public String getIdMedicamento() {
-        return idMedicamento;
-    }
+	
+	public String getIdMedicamento() {
+		return idMedicamento;
+	}
 
-    public void setIdMedicamento(String idMedicamento) {
-        this.idMedicamento = idMedicamento;
-    }
 
-    public String getNombre() {
-        return nombre;
-    }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public void setIdMedicamento(String idMedicamento) {
+		this.idMedicamento = idMedicamento;
+	}
 
-    public String getDosis() {
-        return dosis;
-    }
 
-    public void setDosis(String dosis) {
-        this.dosis = dosis;
-    }
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public String getDosis() {
+		return dosis;
+	}
+
+
+
+	public void setDosis(String dosis) {
+		this.dosis = dosis;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Medicamento [idMedicamento=" + idMedicamento + ", nombre=" + nombre + ", dosis=" + dosis 
+				+  "]";
+	}
+	
 }
