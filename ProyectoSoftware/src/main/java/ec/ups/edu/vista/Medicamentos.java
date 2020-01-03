37a0168d2d24de290a82cb8f29e9fc7f483477e7
DavidEF1996 <59062590+DavidEF1996@users.noreply.github.com>
2020-01-02 20:51:45 -0500

    Creacion de clase y paquete
    Creada la interfaz de medicamentos en un paquete vista

---
+++ ProyectoSoftware/src/main/java/ec/ups/edu/vista/Medicamentos.java
@@ -0,0 +1,42 @@
+package ec.ups.edu.vista;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Medicamentos extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Medicamentos frame = new Medicamentos();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Medicamentos() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
