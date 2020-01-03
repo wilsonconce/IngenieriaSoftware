03295d6dccdd26094a9ec0fe3a479266d505f2ff
DavidEF1996 <59062590+DavidEF1996@users.noreply.github.com>
2020-01-02 21:03:39 -0500

    Modificaciones  Interfaz Medicamento

---
+++ ProyectoSoftware/src/main/java/ec/ups/edu/controlador/ControladorMedicamentos.java
@@ -0,0 +1,5 @@
+package ec.ups.edu.controlador;
+
+public class ControladorMedicamentos {
+
+}
--- ProyectoSoftware/src/main/java/ec/ups/edu/vista/VistaMedicamentos.java
+++ ProyectoSoftware/src/main/java/ec/ups/edu/vista/VistaMedicamentos.java
@@ -7,7 +7,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class Medicamentos extends JFrame {
+public class VistaMedicamentos extends JFrame {
 
 	private JPanel contentPane;
 
@@ -18,7 +18,7 @@ public class Medicamentos extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Medicamentos frame = new Medicamentos();
+					VistaMedicamentos frame = new VistaMedicamentos();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,9 +30,9 @@ public class Medicamentos extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public Medicamentos() {
+	public VistaMedicamentos() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 516, 336);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
