0fe99a8af2f9758e2a6e5ba853e80ede433d52df
LeandroLeonCh <49213028+LeandroLeonCh@users.noreply.github.com>
2020-01-15 12:36:12 -0500

    commit

--- CitasMedicas/src/citas/medicas/Conexion.java
+++ CitasMedicas/src/citas/medicas/Conexion.java
@@ -1,11 +1,14 @@
 package citas.medicas;
 
+
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
 
+<<<<<<< HEAD
 	public static Connection conn;
 	private static final String driver = "com.mysql.jdbc.Driver";
 	private static final String user = "root";
@@ -25,26 +28,44 @@ public class Conexion {
 			if (conn != null) {
 				System.out.println("Conexion establecida..");
 			}
+=======
+		public static Connection conn;
+		private static final String driver = "com.mysql.jdbc.Driver";
+		private static final String user = "root";
+		private static final String password = "";
+		private static final String url = "jdbc:mysql://localhost:3306/citasmedicas";
 
-		} catch (ClassNotFoundException | SQLException e) {
 
-			System.out.println("error al conectar" + e);
+		public Conexion() throws SQLException {
+			conn = null;
+>>>>>>> 6d349f3b598e51d50216e506b699b1148fbcdab3
+
+			try {
+				Class.forName(driver);
+				conn = DriverManager.getConnection(url, user, password);
+				if (conn != null) {
+					System.out.println("Conexion establecida..");
+				}
+
+			} catch (ClassNotFoundException | SQLException e) {
+
+				System.out.println("error al conectar" + e);
+			}
 		}
-	}
 
-	public Connection getConnection() {
-		return conn;
-	}
-
-	public void desconectar() {
-		conn = null;
-		if (conn == null) {
-			System.out.println("Conexion terminada...");
+		public Connection getConnection() {
+			return conn;
 		}
-	}
 
-	public Object conexion() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		public void desconectar() {
+			conn = null;
+			if (conn == null) {
+				System.out.println("Conexion terminada...");
+			}
+		}
+
+		public Object conexion() {
+			// TODO Auto-generated method stub
+			return null;
+		}
 }
